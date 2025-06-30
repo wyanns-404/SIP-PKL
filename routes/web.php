7cac3bd3a7628a7926291fed2bec495c4073d0fa
wyanns-404 <wayansantiearif@gmail.com>
2025-06-30 14:34:01 +0700

    Install Tailwind

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/laravel', function () {
     return view('welcome');
+});
+
+Route::get('/', function () {
+    return view('landingPage');
 });
