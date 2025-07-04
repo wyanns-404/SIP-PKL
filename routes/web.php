cb1f02e68d043b9d1f703faa791577319f8cacf4
wyanns-404 <wayansantiearif@gmail.com>
2025-07-04 10:39:00 +0700

    Migrate jetstream/livewire

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/laravel', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/', function () {
-    return view('landingPage');
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
 });
