ac1cfe693dc4c8195307e4ef4ddc29e4f0327ca1
TehBotolxESSE <creamynald@gmail.com>
2025-07-06 02:59:01 +0700

    (add) test prefix backend

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// prefix admin
+Route::prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('backend.index');
+    })->name('admin.dashboard');
+
+    // Other admin routes can be added here
+});
