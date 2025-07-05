9220c0ea7d488ead5fb00e2d58281a5fab5da4a1
TehBotolxESSE <creamynald@gmail.com>
2025-07-06 03:59:28 +0700

    (fix) route for admin

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+// admin prefix 
+Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
+    Route::get('/', function () {
+        return view('backend.index');
+    })->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    // Add more admin routes here
 });
 
 require __DIR__.'/auth.php';
