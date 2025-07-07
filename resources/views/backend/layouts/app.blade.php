e4594021250a394cbf945c4ad4d41cbbad816e2e
TehBotolxESSE <creamynald@gmail.com>
2025-07-07 09:55:29 +0700

    (chg) app name in layouts

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -19,7 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="apple-touch-icon" sizes="76x76" href="{{asset('soft-ui/assets/img/apple-icon.png')}}" />
     <link rel="icon" type="image/png" href="{{asset('soft-ui/assets/img/favicon.png')}}" />
-    <title>Soft UI Dashboard Tailwind</title>
+    <title>{{ config('app.name', 'SIP-PKL') }}</title>
     <!--     Fonts and icons     -->
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
     <!-- Font Awesome Icons -->
--- resources/views/backend/layouts/sidebar-nav.blade.php
+++ resources/views/backend/layouts/sidebar-nav.blade.php
@@ -4,7 +4,7 @@
         <i class="absolute top-0 right-0 hidden p-4 opacity-50 cursor-pointer fas fa-times text-slate-400 xl:hidden" sidenav-close></i>
         <a class="block px-8 py-6 m-0 text-sm whitespace-nowrap text-slate-700" href="javascript:;" target="_blank">
           <img src="{{asset('soft-ui/assets/img/logo-ct.png')}}" class="inline h-full max-w-full transition-all duration-200 ease-nav-brand max-h-8" alt="main_logo" />
-          <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">Soft UI Dashboard</span>
+          <span class="ml-1 font-semibold transition-all duration-200 ease-nav-brand">{{ config('app.name', 'SIP-PKL') }}</span>
         </a>
       </div>
 
@@ -16,8 +16,8 @@
         </ul>
       </div>
 
-      <div class="mx-4">
-        <!-- load phantom colors for card after: -->
+      <!-- <div class="mx-4">
+       //load phantom colors for card after
         <p class="invisible hidden text-gray-800 text-red-500 text-red-600 after:bg-gradient-to-tl after:from-gray-900 after:to-slate-800 after:from-blue-600 after:to-cyan-400 after:from-red-500 after:to-yellow-400 after:from-green-600 after:to-lime-400 after:from-red-600 after:to-rose-400 after:from-slate-600 after:to-slate-300 text-lime-500 text-cyan-500 text-slate-400 text-fuchsia-500"></p>
         <div class="after:opacity-65 after:bg-gradient-to-tl after:from-slate-600 after:to-slate-300 relative flex min-w-0 flex-col items-center break-words rounded-2xl border-0 border-solid border-blue-900 bg-white bg-clip-border shadow-none after:absolute after:top-0 after:bottom-0 after:left-0 after:z-10 after:block after:h-full after:w-full after:rounded-2xl after:content-['']" sidenav-card>
           <div class="mb-7.5 absolute h-full w-full rounded-2xl bg-cover bg-center" style="background-image: url('{{asset('soft-ui/assets/img/curved-images/white-curved.jpeg')}}')"></div>
@@ -32,7 +32,7 @@
             </div>
           </div>
         </div>
-        <!-- pro btn  -->
+        //pro btn
         <a class="inline-block w-full px-6 py-3 my-4 text-xs font-bold text-center text-white uppercase align-middle transition-all ease-in border-0 rounded-lg select-none shadow-soft-md bg-150 bg-x-25 leading-pro bg-gradient-to-tl from-purple-700 to-pink-500 hover:shadow-soft-2xl hover:scale-102" target="_blank" href="https://www.creative-tim.com/product/soft-ui-dashboard-pro-tailwind?ref=sidebarfree">Upgrade to pro</a>
-      </div>
+      </div> -->
     </aside>
