c02d66381f1fcf61e8b02eef7bb7fdf653f38c7a
TehBotolxESSE <creamynald@gmail.com>
2025-07-07 10:06:12 +0700

    (fix) breadcrumb

--- resources/views/backend/layouts/navbar.blade.php
+++ resources/views/backend/layouts/navbar.blade.php
@@ -2,14 +2,28 @@
       <nav class="relative flex flex-wrap items-center justify-between px-0 py-2 mx-6 transition-all shadow-none duration-250 ease-soft-in rounded-2xl lg:flex-nowrap lg:justify-start" navbar-main navbar-scroll="true">
         <div class="flex items-center justify-between w-full px-4 py-1 mx-auto flex-wrap-inherit">
           <nav>
-            <!-- breadcrumb -->
+            <!-- Breadcrumb -->
             <ol class="flex flex-wrap pt-1 mr-12 bg-transparent rounded-lg sm:mr-16">
               <li class="text-sm leading-normal">
                 <a class="opacity-50 text-slate-700" href="javascript:;">Pages</a>
               </li>
-              <li class="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']" aria-current="page">Dashboard</li>
+
+              @php
+                $segment2 = request()->segment(2);
+                $segment3 = request()->segment(3);
+              @endphp
+
+              @if($segment2)
+                <li class="text-sm pl-2 capitalize leading-normal text-slate-700 before:float-left before:pr-2 before:text-gray-600 before:content-['/']" aria-current="page">
+                  {{ $segment2 }}
+                </li>
+              @endif
             </ol>
-            <h6 class="mb-0 font-bold capitalize">Dashboard</h6>
+
+            <!-- Judul Halaman -->
+            <h6 class="mb-0 font-bold capitalize">
+              {{ $segment3 ?? 'Dashboard' }}
+            </h6>
           </nav>
 
           <div class="flex items-center mt-2 grow sm:mt-0 sm:mr-6 md:mr-0 lg:flex lg:basis-auto">
