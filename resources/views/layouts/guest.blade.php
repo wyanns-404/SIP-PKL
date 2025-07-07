de4a3d737906e05928104e8ed5f75145d8c44d56
wyanns-404 <wayansantiearif@gmail.com>
2025-07-07 11:38:29 +0700

    delete unuse file

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -13,6 +13,8 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @stack('styles')
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
@@ -26,5 +28,7 @@
                 {{ $slot }}
             </div>
         </div>
+
+        @stack('scripts')
     </body>
 </html>
