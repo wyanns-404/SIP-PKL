3db5c79cd8cdba8704cf6a7237f4da8283eee3c7
TehBotolxESSE <creamynald@gmail.com>
2025-07-07 09:23:48 +0700

    (feat) management roles & users

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,14 @@ Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
     Route::get('/', function () {
         return view('backend.index');
     })->name('dashboard');
+    Route::prefix('management')->group(function () {
+        Route::get('roles', function () {
+            return view('backend.management.roles');
+        })->name('roles');
+        Route::get('users', function () {
+            return view('backend.management.users');
+        })->name('users');
+    });
 
     // Add more admin routes here
 });
