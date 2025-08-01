254bd3f425faf30168e7a8298612aa51f1cb7e73
wyanns-404 <wayansantiearif@gmail.com>
2025-08-01 14:35:20 +0700

    (add) system profile manajemen

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -6,10 +6,12 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
-use App\Filament\Pages\Auth\LoginCustom;
-use App\Filament\Pages\Auth\RegisterCustom;
+use Filament\Navigation\MenuItem;
 use Filament\Support\Colors\Color;
+use Illuminate\Support\Facades\Auth;
+use App\Filament\Pages\Auth\LoginCustom;
 use Filament\Http\Middleware\Authenticate;
+use App\Filament\Pages\Auth\RegisterCustom;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Filament\Http\Middleware\AuthenticateSession;
@@ -19,6 +21,9 @@ use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
+use Joaopaulolndev\FilamentEditProfile\Pages\EditProfilePage;
+use Joaopaulolndev\FilamentEditProfile\FilamentEditProfilePlugin;
+
 
 class DashboardPanelProvider extends PanelProvider
 {
@@ -59,6 +64,37 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                FilamentEditProfilePlugin::make()
+                    ->slug('profile')
+                    ->setTitle('Profile')
+                    ->setNavigationLabel('Profile')
+                    ->setIcon('heroicon-o-user-circle')
+                    ->setSort(10)
+                    ->canAccess(fn () => Auth::user()->id === 1)
+                    ->shouldRegisterNavigation(true)
+                    ->shouldShowEmailForm(true)
+                    ->shouldShowDeleteAccountForm(false)
+                    ->shouldShowSanctumTokens(false)
+                    ->shouldShowBrowserSessionsForm(true)
+                    ->shouldShowAvatarForm(
+                        value: true,
+                        directory: 'profile-photos', // image will be stored in 'storage/app/public/avatars
+                        rules: 'mimes:jpeg,png|max:1024'
+                    )
+                    ->shouldShowEditProfileForm(false)
+                    ->customProfileComponents([
+                        \App\Livewire\CustomEditProfileInformationComponent::class,
+                    ])
+
+            ])
+            ->userMenuItems([
+                'profile' => MenuItem::make()
+                    // ->label(fn() => auth()->user()->name)
+                    ->label(fn() => Auth::user()->name)
+                    ->url(fn (): string => EditProfilePage::getUrl())
+                    ->icon('heroicon-m-user-circle'),
             ]);
     }
 }
