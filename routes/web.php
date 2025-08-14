b3fcf6fe633c08c3681eecd8ea3c802b7c6a644d
wyanns-404 <wayansantiearif@gmail.com>
2025-08-14 22:14:49 +0700

    deploy production

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/laravel', function () {
+    return view('welcome');
+});
 
 Route::get('/', function () {
     return view('landing-page');
