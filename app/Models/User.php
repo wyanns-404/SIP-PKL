e4abddd02173e0fcf90c0d3f8f41ee9ec930c69a
wyanns-404 <wayansantiearif@gmail.com>
2025-08-14 22:31:09 +0700

    deploy production

--- app/Models/User.php
+++ app/Models/User.php
@@ -62,6 +62,6 @@ class User extends Authenticatable implements MustVerifyEmail, HasAvatar, Filame
 
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return $this->hasVerifiedEmail();
     }
 }
